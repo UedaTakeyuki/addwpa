98d5bde2476432185fbd7ee7befedb518c3b0f2b
UedaTakeyuki <takeyuki@uedatakeyuki-no-MacBook-Air.local>
2015-09-06 13:48:37 +0900

    Remove unless steps from shell.

--- addwpa.sh
+++ addwpa.sh
@@ -1,11 +1,7 @@
 addwpafile=/boot/addwpa.txt #wpa追加定義ファイル
-wpaconf=/etc/wpa_supplicant/wpa_supplicant.conf
 if [ -e $addwpafile ]; then #wpa追加定義ファイルがあれば
-  echo hello
   ssid=`head -n 1 $addwpafile`
   psk=`tail -n 1 $addwpafile`
-  echo $ssid
-  echo $psk
   if [ -n "$ssid" -a -n "$psk" ]; then #両方とも長さが0でなければ
     # wpa_supplicant.conf の末尾に network 定義を追加
     nn=`wpa_cli add_network | tail -n 1` # 追加した network number
@@ -15,7 +11,5 @@ if [ -e $addwpafile ]; then #wpa追加定義ファイルがあれば
     wpa_cli save_config
     # wpa追加定義ファイル の削除
     rm $addwpafile
-    # reboot
-    #reboot
   fi
 fi

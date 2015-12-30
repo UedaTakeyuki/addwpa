1bb08a2f3090a2f8fa728af99a8807684e047f88
Takeyuki UEDA <takeyuki@MBA.local>
2015-12-30 12:35:29 +0900

    Support new line code of addwpa.txt created by Windows(CRLF) and Mac(CR).

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ Add a pair of wpa key & SSID from /boot/addwpa.txt file, good for Raspberry Pi b
 This script read a pair of setting value of ssid & wpa key from /boot/addwpa.txt file, and add these by wpa_cli. Typically, intended to be called from rc.local.
 /boot/addwpa.txt will be deleted after set by this script.
 
+# Require
+nkf: Convert CRLF(Windows), CR(Mac) to LF(Linux).
+
 # Installing
-1. Download addwpa.sh anywhare, fx: /home/pi
-2. edit /etc/rc.local as follows:
+1. sudo apt-get install nkf
+2. Download addwpa.sh anywhare, fx: /home/pi
+3. edit /etc/rc.local as follows:
 
 ```bash:
 sudo nano /etc/rc.local
--- addwpa.sh
+++ addwpa.sh
@@ -1,15 +1,25 @@
-addwpafile=/boot/addwpa.txt #wpa追加定義ファイル
-if [ -e $addwpafile ]; then #wpa追加定義ファイルがあれば
+#!/bin/sh -eu 
+# http://qiita.com/youcune/items/fcfb4ad3d7c1edf9dc96
+#trap 'echo NG' ERR
+
+addwpafile=/boot/addwpa.txt # path of addwpa.txt.
+wpaconf=/etc/wpa_supplicant/wpa_supplicant.conf
+if [ -e $addwpafile ]; then # exist addwpa.txt file then:
+  nkf -Lu --overwrite $addwpafile # convert CRLF or CR to LF.
+  echo hello
   ssid=`head -n 1 $addwpafile`
-  psk=`tail -n 1 $addwpafile`
-  if [ -n "$ssid" -a -n "$psk" ]; then #両方とも長さが0でなければ
-    # wpa_supplicant.conf の末尾に network 定義を追加
-    nn=`wpa_cli add_network | tail -n 1` # 追加した network number
+  psk=`head -n 2 $addwpafile | tail -n 1`
+  echo $ssid
+  echo $psk
+  if [ -n "$ssid" -a -n "$psk" ]; then # confirm ssid and psk 
+    # add network statement to the end of wpa_supplicant.conf
+    nn=`wpa_cli add_network | tail -n 1` # added network number
     wpa_cli set_network $nn ssid \"$ssid\"
     wpa_cli set_network $nn psk \"$psk\"
     wpa_cli enable_network $nn
     wpa_cli save_config
-    # wpa追加定義ファイル の削除
+    # remove addwpa.txt
     rm $addwpafile
+    # reboot
   fi
 fi
